2d7e27a27a92d13865b6b8d7bdc1cf6fdb8e7a78
lucaragosta90@gmail.com <lucaragosta90@gmail.com>
2023-12-19 15:41:50 +0100

    modifica

--- src/Components/navbar/Navigation.jsx
+++ src/Components/navbar/Navigation.jsx
@@ -10,7 +10,7 @@ const Navigation = () => {
     return (
         <Navbar expand="lg" className="custom-nav">
             <Container fluid>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="/Home">Home</Nav.Link>
